63172f8f5f1210ab9779e54ca3b20f9d4040cec6
AkaiZSK <41275413+AkaiZSK@users.noreply.github.com>
2018-08-03 22:48:30 +0800

    fullscreen_test

--- js/script.js
+++ js/script.js
@@ -106,3 +106,19 @@ $(".fa-chevron-circle-up").on("click", function (e) {
     e.preventDefault();
 });
 
+
+document.fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+}
+
+if (document.fullscreenEnabled) {
+    requestFullscreen(document.documentElement);
+}
